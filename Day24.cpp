afa3a9e20a30ba9cb100aaf0f05d2e1c4e0fb99c
Semih Alperen Kayaaltı <45532883+semihalperen@users.noreply.github.com>
2019-06-26 22:02:36 +0300

    Create Day24.cpp

---
+++ Day24.cpp
@@ -0,0 +1,92 @@
+#include <cstddef>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+          Node* removeDuplicates(Node *head)
+          {
+            Node* traverse = head;
+            Node* traverseAhead = traverse -> next;
+
+            while(traverseAhead){
+                if(traverseAhead->data == traverse->data){
+                    Node* notDuplicate = traverseAhead;
+                
+                    while(notDuplicate && notDuplicate->data == traverse->data)
+                        notDuplicate = notDuplicate -> next;
+                
+                    traverse->next = (notDuplicate == nullptr) ? nullptr : notDuplicate;
+                }
+                if(traverse->next == nullptr)
+                    break;
+                    
+                traverse = traverse -> next;
+                traverseAhead = traverseAhead -> next;
+            }
+            return head;
+          }
+
+          Node* insert(Node *head,int data)
+          {
+               Node* p=new Node(data);
+               if(head==NULL){
+                   head=p;  
+
+               }
+               else if(head->next==NULL){
+                   head->next=p;
+
+               }
+               else{
+                   Node *start=head;
+                   while(start->next!=NULL){
+                       start=start->next;
+                   }
+                   start->next=p;   
+
+               }
+                    return head;
+                
+            
+          }
+          void display(Node *head)
+          {
+                  Node *start=head;
+                    while(start)
+                    {
+                        cout<<start->data<<" ";
+                        start=start->next;
+                    }
+           }
+};
+			
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+    head=mylist.removeDuplicates(head);
+
+	mylist.display(head);
+		
+}
